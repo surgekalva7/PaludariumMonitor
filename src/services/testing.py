5593a0d80518c660ddff9d2a56d09a836fa1d51f
Srujan Kalva <srujankalva@Srujans-MacBook-Pro.local>
2024-12-04 22:30:41 -0700

    Yolo5 creation

--- src/services/testing.py
+++ src/services/testing.py
@@ -1,5 +1,72 @@
 import cv2
-import tensorflow as tf
+from ultralytics import YOLO
+
+model = YOLO("yolov9c.pt")
+
+def predict(chosen_model, img, classes=[], conf=0.5):
+    if classes:
+        results = chosen_model.predict(img, classes=classes, conf=conf)
+    else:
+        results = chosen_model.predict(img, conf=conf)
+
+    return results
+
+def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+    results = predict(chosen_model, img, classes, conf=conf)
+    for result in results:
+        for box in result.boxes:
+            cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
+            cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                        (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+    return img, results
 
 
-API_KEY = "sk-fj2S674cf83d2f3a47847"
+# read the image
+image = cv2.imread("YourImagePath")
+result_img, _ = predict_and_detect(model, image, classes=[], conf=0.5)
+
+
+cv2.imshow("Image", result_img)
+cv2.imwrite("YourSavePath", result_img)
+cv2.waitKey(0)
+
+video_path = r"YourVideoPath"
+cap = cv2.VideoCapture(video_path)
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+    result_img, _ = predict_and_detect(model, img, classes=[], conf=0.5)
+    cv2.imshow("Image", result_img)
+    
+    cv2.waitKey(1)
+
+# defining function for creating a writer (for mp4 videos)
+def create_video_writer(video_cap, output_filename):
+    # grab the width, height, and fps of the frames in the video stream.
+    frame_width = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(video_cap.get(cv2.CAP_PROP_FPS))
+    # initialize the FourCC and a video writer object
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    writer = cv2.VideoWriter(output_filename, fourcc, fps,
+                             (frame_width, frame_height))
+    return writer
+
+output_filename = "YourFilename"
+writer = create_video_writer(cap, output_filename)
+
+video_path = r"YourVideoPath"
+cap = cv2.VideoCapture(video_path)
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+    result_img, _ = predict_and_detect(model, img, classes=[], conf=0.5)
+    writer.write(result_img)
+    cv2.imshow("Image", result_img)
+    
+    cv2.waitKey(1)
+writer.release()
